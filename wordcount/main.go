d918f2fada2b9250bdd323df8f55db91f5937ace
azaiv <a.zakirov@rozentalgroup.ru>
2026-04-08 18:25:03 +0300

    Решены задачи: sum, tour0, wordcount

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,33 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	files := os.Args[1:]
+
+	counts := map[string]int64{}
+
+	for _, file := range files {
+		data, err := os.ReadFile(file)
+		if err != nil {
+			panic(err)
+		}
+
+		lines := strings.Split(string(data), "\n")
+
+		for _, line := range lines {
+			counts[line]++
+		}
+	}
+
+	for line, count := range counts {
+		if count >= 2 {
+			fmt.Printf("%d\t%s\n", count, line)
+		}
+	}
 }
