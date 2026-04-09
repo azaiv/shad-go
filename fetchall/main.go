be280976d42ee0a17fa08d5b4335600ac18d222f
azaiv <a.zakirov@rozentalgroup.ru>
2026-04-09 17:23:10 +0300

    Решение задачи fetchAll

--- fetchall/main.go
+++ fetchall/main.go
@@ -2,6 +2,37 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
 
+func main() {
+	urls := os.Args[1:]
+
+	messageChannel := make(chan string)
+	start := time.Now()
+
+	for _, url := range urls {
+		go func(url string) {
+			resp, err := http.Get(url)
+			if err != nil {
+				messageChannel <- err.Error()
+				return
+			}
+			resp.Body.Close()
+			messageChannel <- url
+		}(url)
+	}
+
+	for range len(urls) {
+		msg := <-messageChannel
+		end := time.Since(start)
+
+		fmt.Println(end, msg)
+	}
+
+	fmt.Println("elapsed", time.Since(start))
 }
