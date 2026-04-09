4d376f5ed922a4167350b264726cb9de1f727e1d
azaiv <a.zakirov@rozentalgroup.ru>
2026-04-09 16:28:08 +0300

    Решение задачи urlfetch

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,29 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func main() {
+	urls := os.Args[1:]
+	for _, url := range urls {
+		resp, err := http.Get(url)
+
+		if err != nil {
+			os.Exit(1)
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+
+		if err != nil {
+			os.Exit(1)
+		}
+
+		fmt.Println(string(body))
+	}
 }
